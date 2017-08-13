e7e93590a6c84918dfb8441a2727c4e090a69e06
Antony Ho <ntonyworkshop@gmail.com>
2017-08-14 00:56:37 +0800

    Add RESTful API stub files and Swagger static files

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,18 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["github.com/antonyho/go-kart/controllers:HealthController"] = append(beego.GlobalControllerRouter["github.com/antonyho/go-kart/controllers:HealthController"],
+		beego.ControllerComments{
+			Method: "Status",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+}
